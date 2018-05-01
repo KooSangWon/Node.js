6c28ab2969944b0f2c2a9c5950a78b90b29903bc
Absoas <abso8787@naver.com>
2018-05-01 17:48:45 +0900

    test

--- myHandler.js
+++ myHandler.js
@@ -1,50 +1,51 @@
 const queryString = require('querystring');
 
+const myBrain = require('./myBrain');
+const myData = require('./twitterDonKim');
+
+let trainedNet = myBrain.train(myData.trainingData);
 
 function sleep(millisec) {
     let timeStart = new Date().getTime();
-    while (new Date().getTime() < timeStart + millisec);//new를 써야 초기화를 해준다?
+    while (new Date().getTime() < timeStart + millisec);
 }
 
-
 function start(res) {
+    console.log('Request handler \'start\'');
     let sBody = '<html>' + '<head>' +
         '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />' +
         '</head>' + '<body>' +
-        '이름과 별명을 입력하세요.<br>' +
+        '내용을 입력하세요.<br>' +
         '<form action="/hello" method="post">' +
         '<input type="text" name="myName" /><br>' +
-        '<input type="text" name="myNick" /><br>' +
         '<button type="submit">입력 완료</button>' +
         '</form>' + '</body>' + '</html>';
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(sBody);
     res.end();
+}
 
-    // res.writeHead(200,{'Content-Type' : 'text/html'});
-    // res.write('Hello, start!');
-    // res.end();
+function execute(input) {
+    let result = myBrain.getResult(trainedNet, input);
+    console.log('trump = ' + Math.round(result.trump * 100) + ', kardashian = ' + Math.round(result.kardashian * 100) + '');
+    let buffer = ('trump = ' + Math.round(result.trump * 100) + ', kardashian = ' + Math.round(result.kardashian * 100) + '');
+    return buffer;
 }
 
 function hello(res, postData) {
-    // //sleep(20000);//20초
-    // setTimeout(function () {//기다리는중에도 접근이 가능함
-
-    // res.writeHead(200,{'Content-Type' : 'text/html'});
-    // res.write('Hello, hello!');
-    // res.end();
-    // }, 20000);
+    console.log('Request handler \'hello\'');
     let sBody = '<html>' + '<head>' +
         '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />' +
         '</head>' + '<body>' +
-        '안녕하세요, ' + queryString.parse(postData).myName +
-        '(별명: ' + queryString.parse(postData).myNick + ')님!' +
+        'Text : ' + queryString.parse(postData).myName +
+        '<br>비율 : ' + execute(postData) +
         '</body>' + '</html>';
+
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(sBody);
+
     res.end();
 }
-
 
 exports.start = start;
 exports.hello = hello;
